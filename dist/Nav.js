d4c7656354aeba96f1695d414972d295e9468d25
dwicao <lutfiandwica@gmail.com>
2017-03-14 08:55:57 +0700

    initial commit

---
+++ dist/Nav.js
@@ -0,0 +1,72 @@
+import React, { PureComponent, PropTypes } from 'react';
+import {
+    View,
+    StyleSheet,
+} from 'react-native';
+
+class Nav extends PureComponent {
+    constructor() {
+        super();
+
+        this._handleTabChange = this._handleTabChange.bind(this);
+    }
+
+    _handleTabChange(tabIndex) {
+        this.props.onTabChange(tabIndex);
+    }
+
+    _containerStyle() {
+        return {
+            flexDirection: 'row',
+            borderTopWidth: StyleSheet.hairlineWidth,
+            backgroundColor: this.props.backgroundColor,
+            borderTopColor: this.props.borderTopColor,
+            height: this.props.height,
+        }
+    }
+    
+    render() {
+        return (
+            <View style={[this._containerStyle(), this.props.style]}>
+                {React.Children.map(this.props.children, (child, tabIndex) => (
+                    React.cloneElement(child, {
+                        tabIndex,
+                        selected: this.props.selected,
+                        activeColor: this.props.activeColor,
+                        unActiveColor: this.props.unActiveColor,
+                        onTabPress: this._handleTabChange,
+                        iconSize: this.props.iconSize,
+                        onlyIcon: this.props.onlyIcon,
+                        pressOpacity: this.props.pressOpacity,
+                        fontSize: this.props.fontSize,
+                    })
+                ))}
+            </View>
+        );
+    }
+}
+
+Nav.defaultProps = {
+    activeColor: 'black',
+    unActiveColor: 'gray',
+    backgroundColor: 'white',
+    borderTopColor: '#DDDDDD',
+    height: 42,
+    iconSize: 22,
+    onlyIcon: false,
+    pressOpacity: 0.7,
+    fontSize: 11,
+};
+
+Nav.propTypes = {
+    backgroundColor: PropTypes.string,
+    borderTopColor: PropTypes.string,
+    height: PropTypes.number,
+    iconSize: PropTypes.number,
+    onlyIcon: PropTypes.bool,
+    pressOpacity: PropTypes.number,
+    fontStyle: PropTypes.object,
+    style: PropTypes.object,
+};
+
+export default Nav;
